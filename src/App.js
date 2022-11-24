259c4f61e732d040fd0e1cae5f6ec93a0c4c67d3
heroes2008 <vgjain@gmail.com>
2022-11-24 17:38:37 -0500

    Initial commit-display decvice list

--- src/App.js
+++ src/App.js
@@ -1,56 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { useDevicesQuery } from "./features/api/apiSlice";
 
 function App() {
+  const { data, isSuccess, isLoading, isFetching, isError, error } =
+    useDevicesQuery();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>SensePlus Devices List</h1>
+      {isLoading && <h2>...Loading</h2>}
+      {isFetching && <h2>...Fetching</h2>}
+      {error && <h2>...Error</h2>}
+      {isSuccess && (
+        <div>
+          {data?.map((device) => {
+            return (
+              <div key={device.id}>
+                <span>{device.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "sensePlusApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://13.232.30.234:8082/api/",
+    prepareHeaders: (headers, { getState }) => {
+      headers.set(
+        "authorization",
+        "Basic YWRtaW5AY2xhcmljZXN5c3RlbXMuaW46QWJjQDEyMw=="
+      );
+    },
+  }),
+  endpoints: (builder) => ({
+    devices: builder.query({
+      query: () => "/devices",
+    }),
+  }),
+});
+
+export const { useDevicesQuery } = apiSlice;
